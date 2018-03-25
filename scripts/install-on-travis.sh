502555589960cef91e0060e2308094052987f502
Peter Odding <peter@peterodding.com>
2018-03-25 17:21:47 +0200

    Try to fix Mac OS X builds on Travis CI

--- scripts/install-on-travis.sh
+++ scripts/install-on-travis.sh
@@ -12,11 +12,8 @@ if [ "$TRAVIS_OS_NAME" = osx ]; then
 fi
 
 # Install the required Python packages.
-pip install pip-accel
-pip-accel install coveralls
-pip-accel install --requirement=requirements.txt
-pip-accel install --requirement=requirements-checks.txt
+pip install --requirement=requirements-travis.txt
 
 # Install the project itself, making sure that potential character encoding
 # and/or decoding errors in the setup script are caught as soon as possible.
-LC_ALL=C pip-accel install .
+LC_ALL=C pip install .
