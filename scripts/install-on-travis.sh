5f067bf321a1509efd0093ccc44ac3b4449ff1a0
Peter Odding <peter@peterodding.com>
2020-02-11 00:14:09 +0100

    Copy MacOS workarounds from 'humanfriendly' package

--- scripts/install-on-travis.sh
+++ scripts/install-on-travis.sh
@@ -1,26 +1,47 @@
 #!/bin/bash -e
 
-# On Mac OS X workers we are responsible for creating the Python virtual
-# environment, because we set `language: generic' in the Travis CI build
-# configuration file (to bypass the lack of Python runtime support).
-if [ "$TRAVIS_OS_NAME" = osx ]; then
-  VIRTUAL_ENV="$HOME/virtualenv/python2.7"
-  if [ ! -x "$VIRTUAL_ENV/bin/python" ]; then
-    if ! which virtualenv &>/dev/null; then
-      # Install `virtualenv' in ~/.local (doesn't require `sudo' privileges).
-      pip install --user virtualenv
-      # Make sure ~/.local/bin is in the $PATH.
-      LOCAL_BINARIES=$(python -c 'import os, site; print(os.path.join(site.USER_BASE, "bin"))')
-      export PATH="$PATH:$LOCAL_BINARIES"
+main () {
+
+  # On Mac OS X workers we are responsible for creating the Python virtual
+  # environment, because we set `language: generic' in the Travis CI build
+  # configuration file (to bypass the lack of Python runtime support).
+  if [ "$TRAVIS_OS_NAME" = osx ]; then
+    local environment="$HOME/virtualenv/python2.7"
+    if [ -x "$environment/bin/python" ]; then
+      msg "Activating virtual environment ($environment) .."
+      source "$environment/bin/activate"
+    else
+      if ! which virtualenv &>/dev/null; then
+        msg "Installing 'virtualenv' in per-user site-packages .."
+        pip install --user virtualenv
+        msg "Figuring out 'bin' directory of per-user site-packages .."
+        LOCAL_BINARIES=$(python -c 'import os, site; print(os.path.join(site.USER_BASE, "bin"))')
+        msg "Prefixing '$LOCAL_BINARIES' to PATH .."
+        export PATH="$LOCAL_BINARIES:$PATH"
+      fi
+      msg "Creating virtual environment ($environment) .."
+      virtualenv "$environment"
+      msg "Activating virtual environment ($environment) .."
+      source "$environment/bin/activate"
+      msg "Checking if 'pip' executable works .."
+      if ! pip --version; then
+        msg "Bootstrapping working 'pip' installation using get-pip.py .."
+        curl -s https://bootstrap.pypa.io/get-pip.py | python -
+      fi
     fi
-    virtualenv "$VIRTUAL_ENV"
   fi
-  source "$VIRTUAL_ENV/bin/activate"
-fi
 
-# Install the required Python packages.
-pip install --requirement=requirements-travis.txt
+  # Install the required Python packages.
+  pip install --requirement=requirements-travis.txt
 
-# Install the project itself, making sure that potential character encoding
-# and/or decoding errors in the setup script are caught as soon as possible.
-LC_ALL=C pip install .
+  # Install the project itself, making sure that potential character encoding
+  # and/or decoding errors in the setup script are caught as soon as possible.
+  LC_ALL=C pip install .
+
+}
+
+msg () {
+  echo "[install-on-travis.sh] $*" >&2
+}
+
+main "$@"
