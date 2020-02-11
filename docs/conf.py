9eebb82c25cd72f64e7582d22fb55932b4c3611e
Peter Odding <peter@peterodding.com>
2020-02-11 22:18:12 +0100

    Bump copyright

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'rotate-backups'
-copyright = '2018, Peter Odding'
+copyright = '2020, Peter Odding'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
